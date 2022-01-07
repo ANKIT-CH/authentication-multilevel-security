d25e85b96d5a842d79d1d8b36493b545a2d105b7
ANKIT-CH <ach1134544@gmail.com>
2022-01-07 16:24:25 +0530

    password encryption by hashing hashing using MD5

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = new express();
 
@@ -22,11 +22,6 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
-userSchema.plugin(encrypt, {
-  secret: process.env.SECRET_KEY,
-  encryptedFields: ["password"],
-});
-
 const User = new mongoose.model("User", userSchema);
 
 app.get("/", function (req, res) {
@@ -39,7 +34,7 @@ app.get("/login", function (req, res) {
 
 app.post("/login", function (req, res) {
   let email = req.body.email;
-  let password = req.body.password;
+  let password = md5(req.body.password);
   User.findOne({ email: email }, function (err, foundUser) {
     if (!err) {
       if (foundUser) {
@@ -62,7 +57,7 @@ app.get("/register", function (req, res) {
 app.post("/register", function (req, res) {
   const newUser = new User({
     email: req.body.email,
-    password: req.body.password,
+    password: md5(req.body.password),
   });
 
   newUser.save(function (err) {
